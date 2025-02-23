9b582a43553c1e3f41c7ac8257077ab9d822c021
Hahns Pena <hahns.pena@gmail.com>
2025-02-22 19:45:09 -0800

    basics: item on ground, movement, stat bar

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+public class Item : MonoBehaviour
+{
+    [SerializeField]private string id;
+    [SerializeField]private string weaponName;
+    [SerializeField]private string description;
+    private Sprite inventorySprite;
+
+    public global::System.String Id { get => id; set => id = value; }
+    public global::System.String WeaponName { get => weaponName; set => weaponName = value; }
+    public global::System.String Description { get => description; set => description = value; }
+    public Sprite InventorySprite { get => inventorySprite; set => inventorySprite = value; }
+
+    void Start(){
+        inventorySprite = GetComponent<Sprite>();
+    }
+
+    public void HoverTextOn(string itemName) {
+        GameObject hoverText = transform.Find("Dropped")?.gameObject.transform.Find("HoverText")?.gameObject;
+        if (hoverText != null) {
+            TMPro.TextMeshProUGUI hoverTextTMP = hoverText.GetComponent<TMPro.TextMeshProUGUI>();
+            hoverTextTMP.text = "[E] " + itemName;
+
+            hoverText.SetActive(true);
+        }
+    }
+
+    public void HoverTextOff() {
+        GameObject hoverText = transform.Find("Dropped")?.gameObject.transform.Find("HoverText")?.gameObject;
+        if (hoverText != null) {
+            hoverText.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scripts/SirGluten.cs
@@ -0,0 +1,96 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+public class SirGluten : MonoBehaviour
+{
+    private Rigidbody2D body;
+    private float verticalInput, horizontalInput;
+    private float speed = 4f;
+
+    // STATS
+    private int health, glucose, yeast, yeastLevel;
+    private int maxHealth=100, maxGlucose=100, maxYeast, maxYeastLevel;
+
+    [SerializeField] private GameObject healthBar, glucoseBar, yeastBar;
+    private Slider healthSlider, glucoseSlider, yeastSlider;
+    private TMPro.TextMeshProUGUI healthText, glucoseText, yeastText;
+
+    // INVENTORY
+    private Item hoveredWeapon;
+    
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+        InitStats();
+        UpdateStats();
+    }
+
+    // Initializes the Stat Values and Slider/Text variables.
+    void InitStats(){
+        health = maxHealth;
+        glucose = maxGlucose;
+        yeast = 0;
+        yeastLevel = 1;
+
+        healthSlider = healthBar.GetComponentInChildren<Slider>();
+        glucoseSlider = glucoseBar.GetComponentInChildren<Slider>();
+        yeastSlider = yeastBar.GetComponentInChildren<Slider>();
+
+        healthText = healthBar.GetComponentInChildren<TMPro.TextMeshProUGUI>();
+        glucoseText = glucoseBar.GetComponentInChildren<TMPro.TextMeshProUGUI>();
+        yeastText = yeastBar.GetComponentInChildren<TMPro.TextMeshProUGUI>();
+    }
+
+    void UpdateStats(){
+        healthSlider.value = health;
+        glucoseSlider.value = glucose;
+        yeastSlider.value = yeast;
+
+        healthText.text = health + "/" + maxHealth;
+        glucoseText.text = glucose + "/" + maxGlucose;
+        yeastText.text = "Level: " + yeastLevel;
+    }
+
+    void Update() {
+        // Movement
+        verticalInput = Input.GetAxisRaw("Vertical");
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+
+        MovePlayer();
+    }
+    void FixedUpdate()
+    {
+        //MovePlayer();
+    }
+
+    void MovePlayer(){
+        body.linearVelocity = new Vector2(horizontalInput,verticalInput).normalized * speed;
+        
+        if (horizontalInput < 0) {
+            Vector2 rotator = new Vector3(transform.rotation.x, 180f);
+            transform.rotation = Quaternion.Euler(rotator);
+        } else if (horizontalInput > 0) {
+            Vector2 rotator = new Vector3(transform.rotation.x, 0f);
+            transform.rotation = Quaternion.Euler(rotator);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collider) {
+        if (collider.gameObject.tag == "Item") {
+            hoveredWeapon = collider.gameObject.transform.parent.GetComponent<Item>();
+            hoveredWeapon.HoverTextOn(hoveredWeapon.WeaponName);
+            Debug.Log(hoveredWeapon.WeaponName);
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collider) {
+        if (collider.gameObject.tag == "Item") {
+            hoveredWeapon.HoverTextOff();
+            hoveredWeapon = null;
+            
+        }
+    }
+}
